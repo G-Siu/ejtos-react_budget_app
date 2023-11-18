82c2bf172251194affbb642f5e491d53433d0c68
Gary Siu <garysiu@outlook.com>
2023-11-18 15:59:32 -0500

    Submit company budget exercise

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -14,31 +18,48 @@ const App = () => {
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {
-                            /* Add Budget component here */
-                        }        
+                <div className='row mt-3'>
+                    {
+                        // Budget component
+                        <div className='col-sm'>
+                            <Budget />
+                        </div>
+                        
+                    }        
 
-                        {
-                            /* Add Remaining component here*/
-                        }        
+                    {
+                        //Remaining component
+                        <div className='col-sm'>
+                            <Remaining />
+                        </div>
+                    }        
 
-                        {
-                            /* Add ExpenseTotal component here */
-                        }        
-                       
-                        {
-                            /* Add ExpenseList component here */
-                        }         
-
-                        {
-                            /* Add ExpenseItem component here */
-                        }        
-
-                        {
-                            /* Add AllocationForm component here under */
-                        }        
-
+                    {
+                        //ExpenseTotal component
+                        <div className='col-sm'>
+                            <ExpenseTotal />
+                        </div>
+                    }        
+                    
+                </div>         
+                <h3 className='mt-3'>Allocation</h3>
+                <div className='row '>
+                    {
+                        //ExpenseList component
+                        <div className='col-sm'>
+                            <ExpenseList component />
+                        </div>
+                    }
+     
+                </div>
+                <h3 className='mt-3'>Change allocation</h3>
+                <div className='row mt-3'>
+                    {
+                        //AllocationForm component
+                        <div className='col-sm'>
+                            <AllocationForm component />
+                        </div>
+                    }        
                 </div>
             </div>
         </AppProvider>
